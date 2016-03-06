895a3f28a20e0646ebfe8ffc0fb60f635f0c3bd4
Konrad Pason <konrad.pason17@gmail.com>
2016-03-06 16:11:09 +0100

    fixing

--- app/models/mem.rb
+++ app/models/mem.rb
@@ -2,4 +2,12 @@ class Mem < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates :name, presence: true
+
+  after_validation(on: :create) do
+  	self.active = false
+  end
+
+  #scope :active, ->{where active:true}
+  #scope :inactive, ->{where active:false}
 end
